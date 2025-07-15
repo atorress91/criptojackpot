9f489ee531513ada054f09dfde77845d3fb03ae0
AndresTS <andres91411@gmail.com>
2025-07-15 15:26:26 -0600

    Update NotificationProvider import path and relocate file structure
    Moved `NotificationProvider` from `src/providers` to `src/components/notification` and updated its import path in `Providers.tsx`.

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -5,7 +5,7 @@ import { I18nextProvider } from 'react-i18next';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import i18n from '@/locales/i18n';
-import { NotificationProvider } from '@/providers/NotificationProvider';
+import { NotificationProvider } from '@/components/notification/NotificationProvider';
 import {ProvidersProps} from "@/interfaces/providersProps";
 
 export default function Providers({ children }: ProvidersProps) {
